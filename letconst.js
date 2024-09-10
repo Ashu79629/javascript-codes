f403cfcdb1bfddd940f1eee9533e187daa5e8100
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:56:37 +0530

    Add files via upload
    let const in js

---
+++ letconst.js
@@ -0,0 +1,25 @@
+// var a = 20
+
+// var a = 30
+// console.log(a)
+
+
+// if(true){
+//     let c = 68
+//     console.log(c)
+
+// }
+
+// console.log(c)
+
+
+// const 
+
+if(true){
+    const e = 56
+    console.log(e)
+
+
+}
+
+console.log(e)

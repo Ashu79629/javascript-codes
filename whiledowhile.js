a66efc4cc319c1a00503a8cf6b422d4f3bdb6e48
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:54:08 +0530

    Add files via upload
    while do while loop in js

---
+++ whiledowhile.js
@@ -0,0 +1,24 @@
+// syntax for while loop
+// while(condition){
+    //    codes here
+// }
+
+// var i = 1
+
+// while(i<=10){
+//     console.log(i);
+//     i++
+//  }
+
+// do while loop
+// syntax for dowhile loop
+// do{
+// codes here
+// }while(condition)
+
+var i = 16
+
+do{
+    console.log(i);
+    i++
+}while(i<=15)

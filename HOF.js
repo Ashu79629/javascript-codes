18ecbfdf6f1fe30c4d1a04f4f611a45c9afbd107
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-11 00:01:18 +0530

    Add files via upload
    hof in js

---
+++ HOF.js
@@ -0,0 +1,26 @@
+// let arr = [1,2,3,4,5,6]
+
+// let squaredNum = []
+
+
+// for(i = 0; i<arr.length; i++){
+//     squaredNum.push(arr[i]*arr[i])
+// }
+
+// console.log(squaredNum)
+
+
+
+// map fucntion
+
+const num = [2,3,4,5,6]
+const squaredNum = num.map((n)=>{
+    return n*n
+
+
+
+})
+console.log(squaredNum)
+
+
+

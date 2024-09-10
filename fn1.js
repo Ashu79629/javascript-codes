79a557f1b8076d26a474b2ebb1ccdf551f933b43
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:55:42 +0530

    Add files via upload

---
+++ fn1.js
@@ -0,0 +1,25 @@
+// basic funtion
+
+
+// syntax 
+// function demo(){
+// codes goes here in console.log
+// }
+// call the function demo()
+
+
+function greet(){
+    console.log("hello")
+}
+greet()
+
+
+
+// parameters and arguments
+
+// function multiply(a, b){  it is a parameters
+    // console.log(a * b)
+
+// }
+
+// multiply(3,5) it is arguments

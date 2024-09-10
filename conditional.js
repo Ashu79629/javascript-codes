693610f311a00e1ffd682b9442b474688b673621
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:50:10 +0530

    Add files via upload
    conditonal statement in js

---
+++ conditional.js
@@ -0,0 +1,20 @@
+// var myscore = 93
+// if(myscore > 90){
+//     console.log("yeh i got correct")
+// }
+// else{
+//     console.log("wrong")
+// }
+
+
+
+
+
+
+
+let num1=2;
+let num2=02;
+if(num1==num2)
+ console.log("true");
+else
+ console.log("false");

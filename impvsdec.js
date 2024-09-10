73f6492bd589f8e864753a2c16b412aad741c44a
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:59:29 +0530

    Add files via upload
    imp vs dec in js

---
+++ impvsdec.js
@@ -0,0 +1,22 @@
+// imperative way of code
+
+// const a = 7
+
+// let isEven;
+
+
+// if(a*a%2==0){
+//     isEven = true
+// }
+
+
+// else{
+//     isEven = false
+// }
+
+// console.log(isEven)
+
+// declarative way of code
+
+const b = (x)=> (x*x%2==0 ? true : false)
+console.log(b(6))

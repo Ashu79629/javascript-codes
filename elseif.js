ec3f71970d38fac059ac5c07c32f94e0871be0c5
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:52:21 +0530

    Add files via upload
    else if in js

---
+++ elseif.js
@@ -0,0 +1,34 @@
+// 90 - 100 - A
+// 80 - 90 - b
+// 70 - 80 - c
+// 60- 70 - d
+// 50 - 60 - f
+
+
+
+var myscore = 50
+
+if(myscore >= 90){
+    console.log("grade - a")
+}
+
+else if(myscore>=80 && myscore<=90){
+    console.log("grade - b")
+}
+
+else if(myscore>=70 && myscore<=80){
+    console.log("grade - c")
+}
+
+else if(myscore>=60 && myscore<=70){
+    console.log("grade - e")
+}
+
+else{
+    console.log("grade - f")
+}
+
+
+    
+
+    

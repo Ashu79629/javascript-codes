4909c452e4b97ef037e0b0150485c2959f69d420
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:58:14 +0530

    Add files via upload
    desarray in js

---
+++ desarray.js
@@ -0,0 +1,19 @@
+let arr = ['hi', 'i', 'am', 'ashu']
+
+// let a = arr[1]
+
+// let b = arr[2]
+
+// console.log(a)
+
+// console.log(b)
+
+
+
+
+let[a,b,c,d] = arr
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)

5d9276722d2ec40e499b4a609cb6b07e65407173
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:48:02 +0530

    Add files via upload
    arr in javascript

---
+++ arr.js
@@ -0,0 +1,31 @@
+
+
+
+// var arr = [0, "ashutosh pandey", 5, "True"]
+
+// console.log(arr)
+
+//  arr[0] = "ashu"
+//  console.log(arr)
+
+// console.log(arr.length)
+
+// arr.methods
+
+// pop method
+var arr2 = [12,56,67,90]
+
+arr2.pop()
+console.log(arr2)
+
+// push method
+arr2.push(100)
+console.log(arr2)
+
+// shift method starting element is remove
+var d = arr2.shift()
+console.log(d)
+console.log(arr2)
+// unshift array add element in starting 
+arr2.unshift(1200)
+console.log(arr2)

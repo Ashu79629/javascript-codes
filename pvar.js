14b22f53d18eeca3575973a2bf1ce1e93792aef0
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:56:16 +0530

    Add files via upload

---
+++ pvar.js
@@ -0,0 +1,27 @@
+// var a = 22
+
+// var a = 34
+
+// console.log(a)
+
+
+
+
+// if(a===34){
+//     var b = 35
+//     console.log(a)
+// }
+
+// console.log(b)
+
+
+
+function number(){
+    var c = 40
+    console.log(c)
+
+}
+number()
+
+
+console.log(c)

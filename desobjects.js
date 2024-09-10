0d3a25487bab9be61aea096ba1e790ea8d3d3e3f
ASHUTOSH PANDEY <163401856+Ashu79629@users.noreply.github.com>
2024-09-10 23:58:50 +0530

    Add files via upload
    des objects in js

---
+++ desobjects.js
@@ -0,0 +1,22 @@
+// let myObj = {
+//     name : "ashutosh",
+//     age : 18,
+//     gender : 'M',
+
+//     address : {
+//         country : "India",
+//         city : "Jaipur"
+//     }
+// }
+
+
+
+// let{name: n, age: a, gender: g, address:{country : c, city: y}} = myObj
+// console.log(n)
+// console.log(a)
+// console.log(g)
+// console.log(c, y)
+
+
+
+console.log(typeof (new (class { class () {} })))
